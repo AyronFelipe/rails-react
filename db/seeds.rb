3e2a4cf99d3148977e691c60eb78add71f8ae440
Ayron Felipe <ayron41@gmail.com>
2020-02-19 20:38:11 -0300

    Install of gemfile faker and seeds to populate database
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+    User.create({
+        name: Faker::Name.name_with_middle ,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
+        gender: Faker::Gender.short_binary_type
+    })
+end
