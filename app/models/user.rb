df10a059b3dd5385b25bbf90cb4c0fd5580ac563
Ayron Felipe <ayron41@gmail.com>
2020-02-19 22:23:41 -0300

    Verification length of password
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
-    validates :password, presence: true
+    validates :password, presence: true, length: {minimum: 8}
 end
