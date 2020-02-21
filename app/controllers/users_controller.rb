c6257b9bfb9ed8e2d191a45e794cf17760aa3cdd
Ayron Felipe <ayron41@gmail.com>
2020-02-21 12:15:57 -0300

    Changing response messages
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,13 @@ class UsersController < ApplicationController
   def index
     @users = User.select("id, email, name, birthday, gender").all
 
-    render json: {status: 'SUCCESS', message: 'Loaded users', data: @users}, status: :ok
+    render json: {status: 'SUCCESS', message: 'Usuários obtidos com sucesso', data: @users}, status: :ok
   end
 
   # GET /users/1
   def show
     @user = User.where(params[:id]).select("id, email, name, birthday, gender").first
-    render json: {status: 'SUCCESS', message: 'Loaded user', data: @user}, status: :ok
+    render json: {status: 'SUCCESS', message: 'Usuário obtido', data: @user}, status: :ok
   end
 
   # POST /users
@@ -19,25 +19,25 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: {status: 'SUCCESS', message:'Saved user'}, status: :created
+      render json: {status: 'SUCCESS', message:'Informações salvas'}, status: :created
     else
-      render json: {status: 'ERROR', message:'User not saved', data: @user.errors}, status: :unprocessable_entity
+      render json: {status: 'ERROR', message:'As informações não foram salvas', data: @user.errors}, status: :unprocessable_entity
     end
   end
   
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: {status: 'SUCCESS', message:'User updated'}, status: :ok
+      render json: {status: 'SUCCESS', message:'Usuário atualizado'}, status: :ok
     else
-      render json: {status: 'ERROR', message:'User not updated', data: @user.errors}, status: :unprocessable_entity
+      render json: {status: 'ERROR', message:'Usuário não atualizado', data: @user.errors}, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
   def destroy
     @user.destroy
-    render json: {status: 'SUCCESS', message: 'User deleted'}, status: :ok
+    render json: {status: 'SUCCESS', message: 'Usuário deletado com sucesso'}, status: :ok
   end
 
   private
